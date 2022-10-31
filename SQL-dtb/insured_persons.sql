502c86de045021fc6a666a7a8a01944ebd4fbd91
Jan Michálek <janmichalek213@gmail.com>
2022-10-31 09:02:24 +0100

    import data from mysql database

--- SQL-dtb/insured_persons.sql
+++ SQL-dtb/insured_persons.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Počítač: localhost
--- Vytvořeno: Stř 12. říj 2022, 11:14
--- Verze serveru: 10.4.22-MariaDB
--- Verze PHP: 8.1.1
+-- Host: localhost
+-- Generation Time: Oct 31, 2022 at 09:01 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,48 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databáze: `insurance_app`
+-- Database: `insurance_app`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `insured_persons`
+-- Table structure for table `insurance`
+--
+
+CREATE TABLE `insurance` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_czech_ci NOT NULL,
+  `amount` int(11) NOT NULL,
+  `subject` varchar(255) COLLATE utf8mb4_czech_ci NOT NULL,
+  `validFrom` date NOT NULL,
+  `validUntil` date NOT NULL,
+  `insuredPersonID` int(11) NOT NULL,
+  `insurerID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+
+--
+-- Dumping data for table `insurance`
+--
+
+INSERT INTO `insurance` (`id`, `type`, `amount`, `subject`, `validFrom`, `validUntil`, `insuredPersonID`, `insurerID`) VALUES
+(1, 'Pojištění vozidla', 650000, 'Škoda Fabia iV', '2022-10-05', '2024-10-25', 1, 1),
+(4, 'Pojištění vozidla', 400000, 'Skoda Rapid', '2022-10-04', '2025-10-22', 3, 1),
+(5, 'Pojištění vozidla', 1200000, 'Volvo V90', '2022-10-04', '2024-06-27', 3, 1),
+(6, 'Životní pojištění', 2000000, 'Vlastník', '2022-10-04', '2023-09-27', 4, 1),
+(7, 'Životní pojištění', 2000000, 'Lenka Jelínková', '2022-10-04', '2025-10-04', 4, 1),
+(8, 'Cestovní pojištění', 500000, 'Itálie', '2022-10-04', '2022-10-14', 4, 1),
+(11, 'Pojištění vozidla', 600000, 'Škoda Fabia iV', '2022-10-04', '2024-11-21', 4, 2),
+(12, 'Životní pojištění', 1000000, 'Vlastník', '2022-10-04', '2026-10-15', 8, 1),
+(13, 'Životní pojištění', 2000000, 'Vlastník', '2022-10-05', '2022-11-10', 1, 1),
+(14, 'Cestovní pojištění', 11111, 'Rusko', '2022-10-05', '2022-10-27', 1, 1),
+(15, 'Pojištění vozidla', 1250000, 'BMW M3', '2022-10-30', '2022-12-31', 3, 1),
+(16, 'Cestovní pojištění', 200000, 'Kypr', '2022-10-30', '2022-12-30', 8, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `insured_persons`
 --
 
 CREATE TABLE `insured_persons` (
@@ -42,7 +77,7 @@ CREATE TABLE `insured_persons` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
 --
--- Vypisuji data pro tabulku `insured_persons`
+-- Dumping data for table `insured_persons`
 --
 
 INSERT INTO `insured_persons` (`id`, `dateRegistration`, `firstName`, `lastName`, `birthdate`, `city`, `address`, `NIP`, `phone`, `email`, `insurerID`) VALUES
@@ -55,25 +90,125 @@ INSERT INTO `insured_persons` (`id`, `dateRegistration`, `firstName`, `lastName`
 (8, '2022-10-08', 'Marian', 'Modrý', '1989-10-08', 'Pardubice', 'Valentova 98', 67543, 654443221, 'modry@seznam.cz', 1),
 (9, '2022-10-12', 'Tomáš', 'Martinek', '1987-06-17', 'Karviná', 'Modrá 11', 78900, 765434231, 'tomas22martinek@seznam.cz', 1);
 
+-- --------------------------------------------------------
+
 --
--- Indexy pro exportované tabulky
+-- Table structure for table `insurers`
+--
+
+CREATE TABLE `insurers` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(255) COLLATE utf8mb4_czech_ci NOT NULL,
+  `lastName` varchar(255) COLLATE utf8mb4_czech_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_czech_ci NOT NULL,
+  `password` varchar(280) COLLATE utf8mb4_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+
+--
+-- Dumping data for table `insurers`
+--
+
+INSERT INTO `insurers` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES
+(1, 'Michal', 'Novák', 'novak@app.cz', '$2y$10$WqBRt5yFz2ibBMyE7hg96OC8pFvbfpFtCMBT6ACEjQOjLZpdzs/vy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_czech_ci NOT NULL,
+  `password` varchar(300) COLLATE utf8mb4_czech_ci NOT NULL,
+  `role` int(11) NOT NULL,
+  `insurer` int(11) DEFAULT NULL,
+  `person` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `role`, `insurer`, `person`) VALUES
+(1, 'novak@app.cz', '$2y$10$WqBRt5yFz2ibBMyE7hg96OC8pFvbfpFtCMBT6ACEjQOjLZpdzs/vy', 1, 1, NULL),
+(4, 'modry@seznam.cz', '$2y$10$pcMZZRTsS0oVSxX0FG8BX.nz7mNLLotmejJR.sJWVXYOiYjjL6lvO', 2, NULL, 8);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indexy pro tabulku `insured_persons`
+-- Indexes for table `insurance`
+--
+ALTER TABLE `insurance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `insuredPersonID` (`insuredPersonID`);
+
+--
+-- Indexes for table `insured_persons`
 --
 ALTER TABLE `insured_persons`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pro tabulky
+-- Indexes for table `insurers`
+--
+ALTER TABLE `insurers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `person` (`person`),
+  ADD KEY `insurer` (`insurer`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pro tabulku `insured_persons`
+-- AUTO_INCREMENT for table `insurance`
+--
+ALTER TABLE `insurance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `insured_persons`
 --
 ALTER TABLE `insured_persons`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `insurers`
+--
+ALTER TABLE `insurers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `insurance`
+--
+ALTER TABLE `insurance`
+  ADD CONSTRAINT `insurance_ibfk_1` FOREIGN KEY (`insuredPersonID`) REFERENCES `insured_persons` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`person`) REFERENCES `insured_persons` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`insurer`) REFERENCES `insurers` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
